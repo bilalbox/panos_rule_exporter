f5ea369beca5aef6323386d1f75555c6aa58aff8
Nasir Bilal <bilalbox@gmail.com>
2018-04-12 12:53:32 -0400

    Removed bootstrap dependency and added upload functionality

--- web_ui/__init__.py
+++ web_ui/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 import flask_excel
 
 app = Flask(__name__)
+app.secret_key = 'my super secret key'
+app.config['UPLOAD_FOLDER'] = '/home/panos/configs/'
 flask_excel.init_excel(app)
-Bootstrap(app)
 
 from web_ui import routes
--- web_ui/routes.py
+++ web_ui/routes.py
@@ -2,13 +2,17 @@ from sys import path
 path.append('..')
 from web_ui import app
 import export
-from flask import render_template, Response, request
+from flask import render_template, flash, redirect, url_for, Response, request
+from werkzeug.utils import secure_filename
 import logging
 import xmltodict
 import flask_excel
+import datetime
+import os
 
 
-PAN_CFG_FILE = 'tests/get_config_panorama.xml'
+UPLOAD_FOLDER = '/home/panos/configs/'
+ALLOWED_EXTENSIONS = set(['txt', 'xml'])
 
 
 @app.route('/')
@@ -17,20 +21,48 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/choose')
-def choose():    
-    with open(PAN_CFG_FILE, 'r') as f:
-        pan_cfg = xmltodict.parse(f.read())['response']['result']
+@app.route('/choose', methods=['GET', 'POST'])
+def choose():
+    PAN_CFG_FILE = f'{UPLOAD_FOLDER}current_config.xml'
+    try:
+        with open(PAN_CFG_FILE, 'r') as f:
+            pan_cfg = xmltodict.parse(f.read())['response']['result']
 
-    dg_list = [
-        a['@name'] for a in pan_cfg['config']
-        ['devices']['entry']['device-group']['entry']
-        ]
-        
-    return render_template('export.html', dg_list=dg_list)
+        dg_list = [
+            a['@name'] for a in pan_cfg['config']
+            ['devices']['entry']['device-group']['entry']
+            ]
+    except FileNotFoundError:
+        flash('Please upload a Panorama config XML before attempting to download.')
+        return render_template('index.html')
+    return render_template('export.html', dg_list=dg_list)        
 
-@app.route("/download")
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file submitted')
+            return redirect(request.url)
+        file = request.files['file']
+
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            flash('File successfully uploaded! Click "Download" to select device-group.')
+            return redirect(url_for('index',
+                                    filename=filename))
+    return render_template('upload.html')
+
+@app.route('/download')
 def download():
+    PAN_CFG_FILE = f'{UPLOAD_FOLDER}current_config.xml'
     chosen_dg = request.args.get("Device Group")
     with open(PAN_CFG_FILE, 'r') as f:
         pan_cfg = xmltodict.parse(f.read())['response']['result']
@@ -94,4 +126,9 @@ def download():
         except BaseException as e:
             logging.exception("UNABLE TO EXPORT RULE {} DUE TO {}".format(r, e))
 
-    return flask_excel.make_response_from_array(rows, "xlsx")
+    # GENERATE STRING BASED ON CURRENT DATE: YYYYMMDD
+    dt_today = datetime.datetime.today()
+    today = dt_today.strftime('%Y%m%d')
+
+    # GENERATE AND RETURN XSLS BASED ON list of rows
+    return flask_excel.make_response_from_array(rows, "xlsx", file_name="{}_{}".format(chosen_dg,today))
